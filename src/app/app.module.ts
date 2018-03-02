82b9afd852e70c86da1d71e6fff765299dad8629
VCalosch <VCalosch@users.noreply.github.com>
2018-03-02 17:31:42 +0100

    x1102 0.1
    - Introduccion Home Playas PHP
    - Rutas
    - Login
    - Vista Principal Coordinador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { APP_ROUTING } from './app.routes';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { NavbarComponent } from './componentes/compartidos/navbar/navbar.component';
 import { FooterComponent } from './componentes/compartidos/footer/footer.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { CoordinadorComponent } from './componentes/coordinador/coordinador.component';
+import { EscogerPlayaComponent } from './componentes/compartidos/escoger-playa/escoger-playa.component';
 
 
 @NgModule({
@@ -13,10 +17,14 @@ import { FooterComponent } from './componentes/compartidos/footer/footer.compone
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    CoordinadorComponent,
+    EscogerPlayaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,1 +1,18 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { NavbarComponent } from './componentes/compartidos/navbar/navbar.component';
+import { FooterComponent } from './componentes/compartidos/footer/footer.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { CoordinadorComponent } from './componentes/coordinador/coordinador.component';
+import { EscogerPlayaComponent } from './componentes/compartidos/escoger-playa/escoger-playa.component';
 
+
+const APP_ROUTES: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'coordinador', component: CoordinadorComponent },
+  { path: 'escogerPlaya', component: EscogerPlayaComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
